267ffa7dd397db89ae3541e841449d475a46c96a
Youky <1911303982@qq.com>
2022-11-09 22:07:56 +0800

    feat: 支持不同方向的坐标轴

--- src/base/axis.ts
+++ src/base/axis.ts
@@ -1,5 +1,9 @@
 import { drawLine } from "./util";
-export const drawAxis = (graph: Graph, width: number, height: number) => {
+export const drawAxisVertical = (
+  graph: Graph,
+  width: number,
+  height: number
+) => {
   const bottom = height - 1;
   const right = width - 1;
   // 原点、坐标轴终点箭头
@@ -9,6 +13,25 @@ export const drawAxis = (graph: Graph, width: number, height: number) => {
 
   // 横轴
   drawLine(graph, "-", [bottom, 1], [bottom, right - 1]);
+
+  // 纵轴
+  drawLine(graph, "|", [1, 0], [bottom - 1, 0]);
+};
+
+export const drawAxisHorizontal = (
+  graph: Graph,
+  width: number,
+  height: number
+) => {
+  const bottom = height - 1;
+  const right = width - 1;
+  // 原点、坐标轴终点箭头
+  graph[0][0] = "+";
+  graph[0][bottom] = "↓";
+  graph[0][right] = "→";
+
+  // 横轴
+  drawLine(graph, "-", [0, 1], [0, right - 1]);
 
   // 纵轴
   drawLine(graph, "|", [1, 0], [bottom - 1, 0]);
